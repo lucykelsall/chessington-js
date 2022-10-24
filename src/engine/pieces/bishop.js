816794047e29449fd3b4f41347cbb03342e00e7d
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-24 16:56:23 +0100

    Bishop test half-finished

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,19 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let arrayOfAvailableMoves = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < GameSettings.BOARD_SIZE - i; j++) {
+                let availableSquare = Square.at(j, i);
+                if (currentSquare.row === availableSquare.row + 1 && currentSquare.col === availableSquare.col + 1) {
+                    arrayOfAvailableMoves.push(availableSquare);
+                }
+            }
+        
+        }
+        return arrayOfAvailableMoves;
     }
+
 }
