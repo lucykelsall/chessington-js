b232b233ff0fa20da64c06cced3c0f787b1442db
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-10-25 14:07:44 +0100

    King tests passing

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,28 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
         super(player);
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+    getAvailableMoves(board) {        
+        let arrayOfAvailableMoves = new Array(0);
+        const currentSquare = board.findPiece(this);
+        const querySquare1 = Square.at(currentSquare.row - 1, currentSquare.col - 1);
+        const querySquare2 = Square.at(currentSquare.row - 1, currentSquare.col);
+        const querySquare3 = Square.at(currentSquare.row - 1, currentSquare.col + 1);
+        const querySquare4 = Square.at(currentSquare.row, currentSquare.col - 1);
+        const querySquare5 = Square.at(currentSquare.row, currentSquare.col + 1);
+        const querySquare6 = Square.at(currentSquare.row + 1, currentSquare.col - 1);
+        const querySquare7 = Square.at(currentSquare.row + 1, currentSquare.col);
+        const querySquare8 = Square.at(currentSquare.row + 1, currentSquare.col + 1);
+        const arrayOfQuerySquares = [querySquare1, querySquare2, querySquare3, querySquare4, querySquare5, querySquare6, querySquare7, querySquare8]
+        for (let i = 0; i < arrayOfQuerySquares.length; i++) {
+            if (board.isPieceOnBoard(arrayOfQuerySquares[i])) {
+                arrayOfAvailableMoves.push(arrayOfQuerySquares[i]);
+            }
+        }
+        return arrayOfAvailableMoves;
     }
 }
