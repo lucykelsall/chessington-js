f46db33354efa52bc31ba983e30ba9103035a6c8
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-10-25 11:58:57 +0100

    Added bishop tests

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -7,18 +7,26 @@ export default class Bishop extends Piece {
         super(player);
     }
 
-    getAvailableMoves(board) {
+    getAvailableMoves(board) {        
         let arrayOfAvailableMoves = new Array(0);
         const currentSquare = board.findPiece(this);
-
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            for (let j = 0; j < GameSettings.BOARD_SIZE - i; j++) {
-                let availableSquare = Square.at(j, i);
-                if (currentSquare.row === availableSquare.row + 1 && currentSquare.col === availableSquare.col + 1) {
-                    arrayOfAvailableMoves.push(availableSquare);
-                }
+        for (let x = 1; x < 8; x++) {
+            let querySquare1 = Square.at(currentSquare.row - x, currentSquare.col - x);
+            if (querySquare1.row >= 0 && querySquare1.row <= 7 && querySquare1.col >= 0 && querySquare1.col <= 7) {
+                arrayOfAvailableMoves.push(querySquare1);
+            }                
+            let querySquare2 = Square.at(currentSquare.row - x, currentSquare.col + x);
+            if (querySquare2.row >= 0 && querySquare2.row <= 7 && querySquare2.col >= 0 && querySquare2.col <= 7) {
+                arrayOfAvailableMoves.push(querySquare2);
             }
-        
+            let querySquare3 = Square.at(currentSquare.row + x, currentSquare.col - x);
+            if (querySquare3.row >= 0 && querySquare3.row <= 7 && querySquare3.col >= 0 && querySquare3.col <= 7) {
+                arrayOfAvailableMoves.push(querySquare3);
+            }
+            let querySquare4 = Square.at(currentSquare.row + x, currentSquare.col + x);
+            if (querySquare4.row >= 0 && querySquare4.row <= 7 && querySquare4.col >= 0 && querySquare4.col <= 7) {
+                arrayOfAvailableMoves.push(querySquare4);
+            }                  
         }
         return arrayOfAvailableMoves;
     }
